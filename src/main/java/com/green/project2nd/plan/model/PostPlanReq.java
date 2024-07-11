cce14645b2377e960cc6f6bdd674f33ec1bdc1e1
hobbang7531 <songyerim1@naver.com>
2024-07-11 18:55:54 +0900

    0711

--- src/main/java/com/green/project2nd/plan/model/PostPlanReq.java
+++ src/main/java/com/green/project2nd/plan/model/PostPlanReq.java
@@ -18,8 +18,11 @@ public class PostPlanReq {
     @Schema(example = "24-07-23", description = "모임 일정 시작 날짜")
     private String planStartDt;
 
-    @Schema(example = "12:00:00", description = "모임 일정 시작 시간")
+    @Schema(example = "12:00", description = "모임 일정 시작 시간")
     private String planStartTime;
+
+    @Schema(example = "대구광역시 중구 1길", description = "모임 장소")
+    private String planLocation;
 
     @Schema(example = "정기 모임", description = "일정 제목")
     private String planTitle;
@@ -27,6 +30,4 @@ public class PostPlanReq {
     @Schema(example = "단체 회식", description = "일정 내용")
     private String planContents;
 
-    @Schema(example = "대구광역시 중구 1길", description = "모임 장소")
-    private String planLocation;
 }
