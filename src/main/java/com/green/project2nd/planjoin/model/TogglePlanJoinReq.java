65222e3b19fa90cfcca438d68449f1a139cb4485
hobbang <songyerim1@naver.com>
2024-07-19 15:46:15 +0900

    0719

--- src/main/java/com/green/project2nd/planjoin/model/TogglePlanJoinReq.java
+++ src/main/java/com/green/project2nd/planjoin/model/TogglePlanJoinReq.java
@@ -1,6 +1,5 @@
 package com.green.project2nd.planjoin.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,5 +11,4 @@ public class TogglePlanJoinReq {
     private Long plmemberPlanSeq;
     @Schema(example = "1", description = "모임 멤버 PK 값")
     private Long memberSeq;
-    @JsonIgnore private Long plmemberMemberSeq;
 }
