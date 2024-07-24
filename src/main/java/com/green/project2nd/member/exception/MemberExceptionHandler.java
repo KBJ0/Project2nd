a44ad493d46a4d1dcd3ec6b6788e34480e0fdf29
first <rpdlvmf7@gmail.com>
2024-07-24 18:03:22 +0900

    0724 1802 add a comment

--- src/main/java/com/green/project2nd/member/exception/MemberExceptionHandler.java
+++ src/main/java/com/green/project2nd/member/exception/MemberExceptionHandler.java
@@ -19,27 +19,81 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class MemberExceptionHandler {
     private final CheckMapper mapper;
 
-
-
-    // R2 U2 D1
+    /** R2(getMemberDetail) U2(updateMemberGb) <br>
+     * 커스텀 에러가 발생하는 경우는 아래와 같다.
+     * @param memberPartySeq 존재하지 않는 모임
+     * @param memberUserSeq 존재하지 않는 유저, 존재하지 않는 멤버 <br><br>
+     *   1-1.존재하지 않는 모임 : <br>
+     *    null - ResultDto.resultDto(HttpStatus.BAD_REQUEST,2, "정보를 제대로 입력해주세요."); <br><br>
+     *   1-2.존재하지 않는 모임 : <br>
+     *    없는 PK - MsgExceptionNull("2,존재하지 않는 모임입니다."); <br><br>
+     *   2-1.존재하지 않는 유저 : <br>
+     *    null - ResultDto.resultDto(HttpStatus.BAD_REQUEST,2, "정보를 제대로 입력해주세요."); <br><br>
+     *   2-2.존재하지 않는 유저 : <br>
+     *    없는 PK값 - MsgExceptionNull("2,존재하지 않는 유저입니다."); <br><br>
+     *   3. 존재하지 않는 멤버 : <br>
+     *    - MsgExceptionNull("2,존재하지 않는 멤버입니다."); <br><br>
+     */
     public void exception(Long memberPartySeq, Long memberUserSeq) {
         exceptionParty(memberPartySeq);
         exceptionUser(memberUserSeq);
-
         if (mapper.checkMemberForPartySeqAndUserSeq(memberPartySeq,memberUserSeq) == 0){throw new MsgExceptionNull("2,존재하지 않는 멤버입니다.");}
     }
-    // U1
+
+    /** U1(updateMember) <br>
+     * 커스텀 에러가 발생하는 경우는 아래와 같다.
+     * @param memberPartySeq 존재하지 않는 모임
+     * @param p 존재하지 않는 유저, 존재하지 않는 멤버 <br><br>
+     *   1-1.존재하지 않는 모임 : <br>
+     *    null - ResultDto.resultDto(HttpStatus.BAD_REQUEST,2, "정보를 제대로 입력해주세요."); <br><br>
+     *   1-2.존재하지 않는 모임 : <br>
+     *    없는 PK - MsgExceptionNull("2,존재하지 않는 모임입니다."); <br><br>
+     *   2-1.존재하지 않는 유저 : <br>
+     *    null - ResultDto.resultDto(HttpStatus.BAD_REQUEST,2, "정보를 제대로 입력해주세요."); <br><br>
+     *   2-2.존재하지 않는 유저 : <br>
+     *    없는 PK값 - MsgExceptionNull("2,존재하지 않는 유저입니다."); <br><br>
+     *   3. 존재하지 않는 멤버 : <br>
+     *    - MsgExceptionNull("2,존재하지 않는 멤버입니다."); <br><br>
+     */
     public void exception(Long memberPartySeq, UpdateMemberReq p) {
         exception(memberPartySeq, p.getMemberUserSeq());
     }
+
     // U3
+
+    /** U2(updateMemberGb) <br>
+     * 커스텀 에러가 발생하는 경우는 아래와 같다.
+     * @param memberPartySeq 존재하지 않는 모임
+     * @param memberLeaderUserSeq 존재하지 않는 유저, 존재하지 않는 멤버
+     *                            , 권한이 없는 유저 <br><br>
+     *   1-1.존재하지 않는 모임 : <br>
+     *    null - ResultDto.resultDto(HttpStatus.BAD_REQUEST,2, "정보를 제대로 입력해주세요."); <br><br>
+     *   1-2.존재하지 않는 모임 : <br>
+     *    없는 PK - MsgExceptionNull("2,존재하지 않는 모임입니다."); <br><br>
+     *   2-1.존재하지 않는 유저 : <br>
+     *    null - ResultDto.resultDto(HttpStatus.BAD_REQUEST,2, "정보를 제대로 입력해주세요."); <br><br>
+     *   2-2.존재하지 않는 유저 : <br>
+     *    없는 PK값 - MsgExceptionNull("2,존재하지 않는 유저입니다."); <br><br>
+     *   3. 존재하지 않는 멤버 : <br>
+     *    - MsgExceptionNull("2,존재하지 않는 멤버입니다."); <br><br>
+     *   4. 권한이 없는 유저 : <br>
+     *    - MsgExceptionNull("2,권한이 없는 유저입니다."); <br><br>
+     */
     public void exceptionLeader(Long memberPartySeq, Long memberLeaderUserSeq) {
         exception(memberPartySeq,memberLeaderUserSeq);
         if (mapper.checkPartyLeader(memberPartySeq, memberLeaderUserSeq) != 1){
             throw new MsgException("2,권한이 없는 유저입니다.");
         }
     }
-    // R1
+
+    /** R1(getMember) <br>
+     * 커스텀 에러가 발생하는 경우는 아래와 같다.
+     * @param memberPartySeq 존재하지 않는 모임 <br><br>
+     *   1-1.존재하지 않는 모임 : <br>
+     *    null - ResultDto.resultDto(HttpStatus.BAD_REQUEST,2, "정보를 제대로 입력해주세요."); <br><br>
+     *   1-2.존재하지 않는 모임 : <br>
+     *    없는 PK - MsgExceptionNull("2,존재하지 않는 모임입니다."); <br><br>
+     */
     public void exceptionParty(Long memberPartySeq) {
         if (memberPartySeq == null || memberPartySeq == 0) {throw new NullReqValue();}
         if (mapper.checkPartySeq(memberPartySeq) == 0) {throw new MsgExceptionNull("2,존재하지 않는 모임입니다.");}
